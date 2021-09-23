5f620534a1bf1f68e0b8919ddcb6cf3751c3fd98
xiewenli <1223260658@qq.com>
2021-09-23 18:44:42 +0800

    初始化布局

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>
-    <h1>Vue组件</h1>
-  </div>
+  <el-container>
+    <el-header>
+      <Header></Header>
+    </el-header>
+    <el-main>
+      <div>Main</div>
+    </el-main>
+    <el-footer>
+      <Footer></Footer>
+    </el-footer>
+  </el-container>
 </template>
 <script>
 export default {}
 </script>
-<style lang="less" scoped></style>
+
+<style lang="less" scoped>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入element ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// css初始化样式
+import './assets/css/base.css'
+
+// 组件
+import Header from './components/Header/Header.vue'
+import Footer from './components/Footer/Footer.vue'
+
+Vue.component('Header', Header)
+Vue.component('Footer', Footer)
+
+// Vue插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
